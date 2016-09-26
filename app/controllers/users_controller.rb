b082cc1184367e5718874465179530c73cfc4709
ElaineMou <elaine.g.mou@gmail.com>
2016-09-26 03:56:04 +0000

    first changes

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,44 @@
+class UsersController < ApplicationController
+
+  def user_params
+    params.require(:user).permit(:user_id, :email)
+  end
+
+  def show
+    id = params[:user_id] # retrieve user ID from URI route
+    @user = User.find(id) # look up movie by unique ID
+    # will render app/views/movies/show.<extension> by default
+  end
+
+  def index
+    @users = User.all
+  end
+
+  def edit
+    @user = User.find params[:user_id]
+  end
+
+  def update
+    @user = User.find params[:user_id]
+    @user.update_attributes!(user_params)
+    flash[:notice] = "#{@user.user_id} was successfully updated."
+    redirect_to users_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @user.destroy
+    flash[:notice] = "User '#{@user.user_id}' deleted."
+    redirect_to users_path
+  end
+  
+  def new
+       
+  end
+   
+  def create
+    @user = User.create!(user_params)
+    flash[:notice] = "#{@user.user_id} was successfully created."
+    redirect_to users_path
+  end
+end
